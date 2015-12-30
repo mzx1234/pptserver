43564b0e1cbb2241b8a8ef72e211f6c47403e7c0
mzx1234 <316701116@qq.com>
2015-12-30 17:02:55 +0800

    commit 3 interfaces

---
+++ src/main/java/com/gdut/pptserver/service/IPService.java
@@ -0,0 +1,8 @@
+package com.gdut.pptserver.service;
+
+/**
+ * IP相关服务
+ * Created by zison on 2015/12/30.
+ */
+public interface IPService {
+}
---
+++ src/main/java/com/gdut/pptserver/service/ParseService.java
@@ -0,0 +1,8 @@
+package com.gdut.pptserver.service;
+
+/**
+ * 解析PPT文件服务
+ * Created by zison on 2015/12/30.
+ */
+public interface ParseService {
+}
