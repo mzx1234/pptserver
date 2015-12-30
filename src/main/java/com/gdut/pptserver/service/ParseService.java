07597930783d9ef2b85ea464a894cb9ef8adbeaa
mzx1234 <316701116@qq.com>
2015-12-30 18:15:23 +0800

    12.30  18:15

--- src/main/java/com/gdut/pptserver/service/ParseService.java
+++ src/main/java/com/gdut/pptserver/service/ParseService.java
@@ -1,8 +1,18 @@
 package com.gdut.pptserver.service;
 
+import com.gdut.pptserver.exception.ServiceException;
+
 /**
  * 解析PPT文件服务
  * Created by zison on 2015/12/30.
  */
 public interface ParseService {
+
+
+    /**
+     * 解析对应的PPT文件
+     * @param file 文件路径
+     * @throws ServiceException
+     */
+    public void parsePPTFile(String file) throws ServiceException;
 }
---
+++ src/main/java/com/gdut/pptserver/service/impl/ParseServiceImpl.java
@@ -0,0 +1,15 @@
+package com.gdut.pptserver.service.impl;
+
+import com.gdut.pptserver.exception.ServiceException;
+import com.gdut.pptserver.service.ParseService;
+
+/**
+ * 解析PPT文件服务实现类
+ * Created by zison on 2015/12/30.
+ */
+public class ParseServiceImpl implements ParseService {
+    @Override
+    public void parsePPTFile(String file) throws ServiceException {
+
+    }
+}
